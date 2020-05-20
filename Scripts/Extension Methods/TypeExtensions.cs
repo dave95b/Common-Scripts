61a58e4dfc0d03746d75d260672f6cbdddbc117b
Dave <dave95.b@gmail.com>
2020-05-20 18:04:24 +0200

    Add TypeExtensions

---
+++ Scripts/Extension Methods/TypeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class TypeExtensions
+{
+    public static bool HasAttribute<T>(this Type type) where T : Attribute
+        => type.IsDefined(typeof(T), true);
+
+    public static bool Is<T>(this Type type) where T : class
+        => typeof(T).IsAssignableFrom(type);
+}
